deda24574d18aa469fa497e798ed810a83f2caa4
nickdrummond <nickdrummond@yahoo.com>
2018-01-15 15:05:11 +0000

    Uppercase img & sound extensions supported

--- src/main/java/org/coode/www/service/MediaService.java
+++ src/main/java/org/coode/www/service/MediaService.java
@@ -3,20 +3,21 @@ package org.coode.www.service;
 import org.semanticweb.owlapi.model.IRI;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 public class MediaService {
 
+    private List<String> imgs = Arrays.asList(".png",  ".jpg", ".jpeg", ".gif");
+    private List<String> sounds = Arrays.asList(".mp3",  ".wav");
+
     public boolean isImageURL(IRI iri) {
-        String iriStr = iri.toString();
-        return iriStr.endsWith(".png") ||
-                iriStr.endsWith(".gif") ||
-                iriStr.endsWith(".jpg") ||
-                iriStr.endsWith(".jpeg");
+        String iriStr = iri.toString().toLowerCase();
+        return imgs.stream().anyMatch(iriStr::endsWith);
     }
 
     public boolean isSoundURL(IRI iri) {
-        String iriStr = iri.toString();
-        return iriStr.endsWith(".mp3") ||
-                iriStr.endsWith(".wav");
+        String iriStr = iri.toString().toLowerCase();
+        return sounds.stream().anyMatch(iriStr::endsWith);
     }
 }
