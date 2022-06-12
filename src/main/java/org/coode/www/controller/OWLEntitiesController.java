8c9de4ab2aea1e62679606424622c9f49825e636
nickdrummond <nickdrummond@yahoo.com>
2022-06-12 20:11:30 +0100

    Search for annotation values - used in reverse Wookipedia lookup bookmarklet

--- src/main/java/org/coode/www/controller/OWLEntitiesController.java
+++ src/main/java/org/coode/www/controller/OWLEntitiesController.java
@@ -1,18 +1,25 @@
 package org.coode.www.controller;
 
 import org.coode.www.exception.OntServerException;
-import org.coode.www.kit.OWLHTMLKit;
+import org.coode.www.model.Characteristic;
+import org.coode.www.model.OWLObjectWithOntology;
 import org.coode.www.model.SearchResult;
 import org.coode.www.model.SearchResults;
+import org.coode.www.renderer.OWLHTMLRenderer;
 import org.coode.www.service.NameService;
 import org.coode.www.service.SearchService;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value="/entities")
@@ -39,5 +46,41 @@ public class OWLEntitiesController extends ApplicationController {
         }
 
         return results;
+    }
+
+    @RequestMapping(value="annotation", method=RequestMethod.GET)
+    public String findAnnotation(
+            @RequestParam final String search,
+            @RequestParam(required=false) final Optional<String> property,
+            final Model model) throws OntServerException {
+
+        Optional<OWLAnnotationProperty> optAnnot = property.map(p -> kit.getOWLEntityChecker().getOWLAnnotationProperty(p));
+
+        if (property.isPresent() && optAnnot.isEmpty()) {
+            throw new OntServerException("Unknown property: " + property.get());
+        }
+
+        List<OWLObjectWithOntology> results = service.findByAnnotation(search, optAnnot, kit);
+
+        if (results.size() == 1) {
+            OWLObject owlObject = results.get(0).getOWLObject();
+            if (owlObject instanceof OWLAnnotationAssertionAxiom) {
+                OWLObject o = service.getEntities(((OWLAnnotationAssertionAxiom)owlObject).getSubject().asIRI().get(), kit).iterator().next();
+                return "redirect:" + kit.getURLScheme().getURLForOWLObject(o);
+            }
+        }
+
+        OWLHTMLRenderer owlRenderer = new OWLHTMLRenderer(kit, Optional.empty());
+
+        String propLabel = property.orElse("All annotations");
+
+        Characteristic resultsCharacteristic = new Characteristic(null, propLabel, results);
+
+        model.addAttribute("property", propLabel);
+        model.addAttribute("search", search);
+        model.addAttribute("results", resultsCharacteristic);
+        model.addAttribute("mos", owlRenderer);
+
+        return "searchresults";
     }
 }
--- src/main/java/org/coode/www/service/SearchService.java
+++ src/main/java/org/coode/www/service/SearchService.java
@@ -1,15 +1,13 @@
 package org.coode.www.service;
 
-import org.coode.owl.mngr.OWLEntityFinder;
 import org.coode.www.kit.OWLHTMLKit;
-import org.semanticweb.owlapi.model.OWLEntity;
+import org.coode.www.model.OWLObjectWithOntology;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.model.parameters.Imports;
 import org.semanticweb.owlapi.util.ShortFormProvider;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -27,7 +25,7 @@ public class SearchService {
         }
 
         String searchStr = (input.endsWith(wildcard)) ? ".*" + input.replace("\\" + wildcard, ".*") :
-                        ".*" + input + ".*";
+                ".*" + input + ".*";
 
         // Sort first by index of search and then alphabetically
         Comparator<OWLEntity> c = new Comparator<>() {
@@ -42,8 +40,7 @@ public class SearchService {
                 int i2 = sf2.indexOf(str);
                 if (i1 == i2) {
                     return sf1.compareTo(sf2);
-                }
-                else if (i1 < i2) {
+                } else if (i1 < i2) {
                     return -1;
                 }
                 return 1;
@@ -52,4 +49,35 @@ public class SearchService {
 
         return kit.getFinder().getOWLEntities(searchStr).stream().sorted(c).collect(Collectors.toList());
     }
+
+    public List<OWLObjectWithOntology> findByAnnotation(String value, Optional<OWLAnnotationProperty> prop, OWLHTMLKit kit) {
+        Set<OWLObjectWithOntology> results = new HashSet<>();
+        for (OWLOntology ont : kit.getOWLOntologyManager().getOntologies()) {
+            for (OWLAnnotationAssertionAxiom ax : ont.getAxioms(AxiomType.ANNOTATION_ASSERTION)) {
+                if (prop.isEmpty() || prop.get().equals(ax.getProperty())) {
+                    OWLAnnotationValue v = ax.getValue();
+                    if (v.isLiteral() && v.asLiteral().isPresent() && v.asLiteral().get().getLiteral().contains(value)) {
+                        results.add(new OWLObjectWithOntology(ax, ont));
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(results);
+    }
+
+    public Set<OWLEntity> getEntities(IRI iri, OWLHTMLKit kit) {
+        OWLDataFactory df = kit.getOWLOntologyManager().getOWLDataFactory();
+        OWLOntology activeOnt = kit.getActiveOntology();
+        return EntityType.values().stream()
+                .map(t -> {
+                    OWLEntity e = df.getOWLEntity(t, iri);
+                    if (activeOnt.containsEntityInSignature(e, Imports.INCLUDED)) {
+                        return e;
+                    }
+                    return null;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+    }
+
 }
