ba45acb92c01ef893332540849e516cfeb51d817
nickdrummond <nickdrummond@yahoo.com>
2023-02-10 18:50:33 +0000

    Removed System.out call

--- src/main/java/org/coode/www/configuration/KitConfig.java
+++ src/main/java/org/coode/www/configuration/KitConfig.java
@@ -7,6 +7,8 @@ import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +18,13 @@ import java.net.URI;
 @Configuration
 public class KitConfig {
 
+    protected final Logger logger = LoggerFactory.getLogger(KitConfig.class);
+
     @Bean
     public OWLHTMLKit owlhtmlKit(@Value("${ontology.root.location}")   URI root,
                                  @Value("${renderer.annotation.uri}")  URI labelURI,
                                  @Value("${renderer.annotation.lang}") String labelLang) throws OWLOntologyCreationException {
-        System.out.println("Loading Kit... " + root);
+        logger.info("Loading Kit... " + root);
         OntologyLoader loader = new OntologyLoader();
         OWLOntologyManager mngr = OWLManager.createOWLOntologyManager();
         OWLOntology ont = loader.loadOntologies(mngr, root);
