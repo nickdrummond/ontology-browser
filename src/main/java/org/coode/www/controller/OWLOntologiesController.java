645be1e49fcc3d255c5f6313bec5c518df98d3b1
drummondn1 <n.drummond@scibite.com>
2024-02-06 21:47:04 +0000

    Added axioms into the main nav + basic search UI Fixes #18

--- src/main/java/org/coode/www/controller/OWLOntologiesController.java
+++ src/main/java/org/coode/www/controller/OWLOntologiesController.java
@@ -1,14 +1,10 @@
 package org.coode.www.controller;
 
 import com.google.common.net.HttpHeaders;
-import org.apache.commons.io.Charsets;
 import org.apache.commons.io.output.WriterOutputStream;
 import org.coode.www.exception.NotFoundException;
 import org.coode.www.model.Tree;
-import org.coode.www.model.characteristics.Characteristic;
-import org.coode.www.renderer.ElementRenderer;
 import org.coode.www.renderer.OWLHTMLRenderer;
-import org.coode.www.service.OWLAxiomService;
 import org.coode.www.service.OWLOntologiesService;
 import org.coode.www.service.hierarchy.OWLOntologyHierarchyService;
 import org.semanticweb.owlapi.formats.RDFXMLDocumentFormat;
@@ -20,18 +16,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStream;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Optional;
-import java.util.Set;
-
-import static org.coode.www.renderer.HighlightingHTMLRenderer.getHighlightRenderer;
 
 @Controller
 @RequestMapping(value = "/ontologies")
@@ -39,9 +29,6 @@ public class OWLOntologiesController extends ApplicationController {
 
     @Autowired
     private OWLOntologiesService service;
-
-    @Autowired
-    private OWLAxiomService axiomService;
 
     @Autowired
     private OntologyIRIShortFormProvider sfp;
@@ -102,42 +89,5 @@ public class OWLOntologiesController extends ApplicationController {
         } catch (OWLOntologyStorageException e) {
             throw new RuntimeException(e);
         }
-    }
-
-
-    @GetMapping(value = "/{ontId}/axioms/")
-    public String axioms(
-            final Model model,
-            @PathVariable final String ontId,
-            @RequestParam(required = false) Optional<String> search,
-            @RequestParam(required = false, defaultValue = "true") boolean includeImports,
-            @RequestParam(required = false, defaultValue = "20") int pageSize,
-            @RequestParam(required = false, defaultValue = "1") int start
-    ) throws NotFoundException {
-
-        OWLOntology ont = service.getOntologyFor(ontId, kit);
-
-        Set<OWLOntology> onts = includeImports ? ont.getImportsClosure() : Collections.singleton(ont);
-
-        search.ifPresent(s -> {
-            // Prevent injection attacks
-            if (s.contains("<") || s.contains(">") || s.contains("%")) {
-                throw new IllegalArgumentException("Search terms may be text only");
-            }
-        });
-
-        ElementRenderer<OWLObject> owlRenderer = search
-                .map(s -> getHighlightRenderer(s, rendererFactory.getRenderer(ont)))
-                .orElse(rendererFactory.getRenderer(ont));
-
-        Characteristic axioms = search
-                .map(s -> axiomService.findAxioms(search.get(), onts, kit.getShortFormProvider(), start, pageSize))
-                .orElse(axiomService.getAxioms(onts, start, pageSize));
-
-        model.addAttribute("title", axioms.getName());
-        model.addAttribute("axioms", axioms);
-        model.addAttribute("mos", owlRenderer);
-
-        return "axioms";
     }
 }
