6d64187f2b7f0d68a379bdf0c63df338af850823
drummondn1 <n.drummond@scibite.com>
2024-06-21 16:27:59 +0100

    Add leader-line connections

--- src/main/webapp/static/js/timeline.js
+++ src/main/webapp/static/js/timeline.js
@@ -1,9 +1,27 @@
+
+
 $(document).ready(function(){
-    if(document.getElementById('timeline')) {
-        init();
+    let timeline = document.getElementsByClassName('events');
+    if(timeline.length > 0) {
+        init(timeline[0]);
     }
 });
 
-function init() {
-    console.log("init");
+function init(timeline) {
+    timeline.querySelectorAll("li.event.dot.faint").forEach(faint => {
+        const iri = faint.getAttribute("data");
+        timeline.querySelectorAll(`li.event.dot[data="${iri}"]`).forEach(target => {
+            if (target !== faint) {
+                new LeaderLine(faint, target, {
+                    color: '#4189a0',
+                    size: 2,
+                    dash: true,
+                    startSocket: "right",
+                    endSocket: "left",
+                    startSocketGravity: [200, 200],
+                    endSocketGravity: [-200, -200],
+                });
+            }
+        })
+    })
 }
