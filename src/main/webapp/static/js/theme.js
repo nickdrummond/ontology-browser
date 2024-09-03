6500e5e1bf8a43f3080f651f26794f755d1bdf3c
drummondn1 <n.drummond@scibite.com>
2024-09-03 23:13:22 +0100

    Removed dark mode flicker on load Refactored classToggler into theme

--- src/main/webapp/static/js/theme.js
+++ src/main/webapp/static/js/theme.js
@@ -2,32 +2,31 @@ export const theme = (normal, alt, attribute, key) => {
 
     let component;
 
-    function not(lang) {
-        return (lang === alt) ? normal : alt;
+    function not(theme) {
+        return (theme === alt) ? normal : alt;
     }
 
     function attachTo(selector) {
         component = document.querySelector(selector);
-        const lang = retrieve();
-        component.addEventListener("click", () => setLanguage(not(retrieve())));
-        toggle(lang);
+        component.addEventListener("click", () => setTheme(not(retrieve())));
+        toggle(retrieve());
     }
 
-    function toggle(lang) {
-        const notLang = not(lang);
-        component.classList.add(notLang);
-        component.classList.remove(lang);
-        component.innerHTML = notLang;
+    function toggle(theme) {
+        const notTheme = not(theme);
+        component.classList.add(notTheme);
+        component.classList.remove(theme);
+        component.innerHTML = notTheme;
     }
 
-    function setLanguage(lang) {
-        store(lang);
-        document.documentElement.setAttribute(attribute, lang);
-        toggle(lang);
+    function setTheme(theme) {
+        document.documentElement.setAttribute(attribute, theme);
+        store(theme);
+        toggle(theme);
     }
 
-    function store(lang) {
-        sessionStorage.setItem(key, lang);
+    function store(theme) {
+        sessionStorage.setItem(key, theme);
     }
 
     function retrieve() {
