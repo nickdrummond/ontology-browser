d02637ae73d144fd490032807e15e9a1da14a165
drummondn1 <n.drummond@scibite.com>
2024-11-25 23:31:35 +0000

    Cytoscape graph controls started. Depth not quite right

--- src/main/webapp/static/js/graph-cytoscape.js
+++ src/main/webapp/static/js/graph-cytoscape.js
@@ -1,90 +1,115 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    function buildGraph(elements, type) {
-        console.log("Building ", type, " graph");
-
-        var cy = cytoscape({
-            container: document.querySelector('.graph'), // container to render in
-
-            elements: elements,
-
-            style: [
-                {
-                    selector: 'node',
-                    style: {
-                        'background-color': '#2B65EC',
-                        'label': 'data(label)'
-                    }
-                },
-
-                {
-                    selector: ':parent',
-                    style: {
-                        'background-opacity': 0.02,
-                        'border-color': '#2B65EC'
-                    }
-                },
-
-                {
-                    selector: 'edge',
-                    style: {
-                        'line-color': 'rgba(175,175,175,0.12)',
-                        'width': 10,
-                        'curve-style': 'straight-triangle',
-                        'label': 'data(label)'
-                    }
-                },
-
-                {
-                    selector: 'node:selected',
-                    style: {
-                        'background-color': '#F08080',
-                        'border-color': 'red'
-                    }
-                },
-
-                {
-                    selector: 'edge:selected',
-                    style: {
-                        'line-color': '#F08080'
-                    }
-                }
-            ],
-            layout: {
-                name: type,
-                quality: 'default',
-                animate: false,
-                fit: true,
-                idealEdgeLength: 100,
-                // nodeRepulsion: 4500,
-                // edgeElasticity: 0.45,
-                // nestingFactor: 0.1,
-                // gravity: 0.25,
-                // gravityRange: 3.8,
-                // gravityCompound: 1,
-                // gravityRangeCompound: 1.5,
-                // numIter: 500,
-                // tilingPaddingVertical: 10,
-                // tilingPaddingHorizontal: 10,
-                // initialEnergyOnIncremental: 0.3,
-                // step: "all"
-            },
-        });
+    let layout = {
+        name: 'fcose',
+        quality: 'default',
+        animate: true,
+        fit: true,
+        idealEdgeLength: 100,
     }
 
-    const myHeaders = new Headers();
-    myHeaders.append("Accept", "application/json");
-    const urlParams = new URLSearchParams(window.location.search);
-    let type = urlParams.get("type");
-    if (!type) {
-        type = "fcose";
-    }
-    fetch('/graph/data?' + urlParams.toString(), {
-        headers: myHeaders,
-    })
-        .then(response => {
-            response.json().then(json => {
-                buildGraph(json.elements, type);
+    let style = [
+        {
+            selector: 'node',
+            style: {
+                'background-color': '#2B65EC',
+                'label': 'data(label)'
+            }
+        },
+
+        {
+            selector: ':parent',
+            style: {
+                'background-opacity': 0.02,
+                'border-color': '#2B65EC'
+            }
+        },
+
+        {
+            selector: 'edge',
+            style: {
+                'line-color': 'rgba(175,175,175,0.12)',
+                'width': 10,
+                'curve-style': 'straight-triangle',
+                'label': 'data(label)'
+            }
+        },
+
+        {
+            selector: 'node:selected',
+            style: {
+                'background-color': '#F08080',
+                'border-color': 'red'
+            }
+        },
+
+        {
+            selector: 'edge:selected',
+            style: {
+                'line-color': '#F08080'
+            }
+        }
+    ];
+
+    let cy = null;
+
+    let urlParams = new URLSearchParams(window.location.search);
+
+    setupControl("type", (newValue) => {
+        layout.name = newValue;
+        cy.layout(layout).run();
+    });
+
+    setupControl("depth", newValue => {
+        reload();
+    });
+
+    function reload() {
+        console.log("Reloading");
+        const myHeaders = new Headers();
+        myHeaders.append("Accept", "application/json");
+        fetch('/graph/data?' + urlParams.toString(), {
+            headers: myHeaders,
+        })
+            .then(response => {
+                response.json().then(json => {
+                    buildGraph(json.elements);
+                });
             });
-        });
+    }
+
+    function setupControl(name, changed) {
+        const typeControl = document.getElementById(name);
+        if (typeControl) {
+            const type = urlParams.get(name);
+            if (type) {
+                typeControl.value = type;
+            }
+            typeControl.addEventListener('change', function () {
+                const params = new URLSearchParams(window.location.search);
+                params.set(name, this.value);
+                window.history.pushState({}, '', window.location.pathname + '?' + params);
+
+                if (cy) {
+                    changed(this.value);
+                }
+            });
+        }
+    }
+
+    function buildGraph(elements) {
+        // if (cy == null) {
+            cy = cytoscape({
+                container: document.querySelector('.graph'), // container to render in
+                elements: elements,
+                style: style,
+                layout: layout,
+            });
+        // }
+        // else {
+        //     cy.elements = elements;
+        // }
+    }
+
+    reload();
 });
