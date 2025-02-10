b81fde9aaad2f7e5f6e0b97b17cef58bb53fd55b
drummondn1 <n.drummond@scibite.com>
2025-02-10 21:09:07 +0000

    Secure all paths under admin

--- src/main/java/org/ontbrowser/www/feature/admin/SecurityConfig.java
+++ src/main/java/org/ontbrowser/www/feature/admin/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/admin").authenticated()
+                        .requestMatchers("/admin/**").authenticated()
                         .anyRequest().permitAll()
                 )
                 .httpBasic(withDefaults())
